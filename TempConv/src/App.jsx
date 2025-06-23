fdea02bae95823b22cee89bf37cbadd2e3050385
Alfiya <al.yausheva@gmail.com>
2025-06-23 22:38:49 +0300

    ⊗jsrtPmHkUEGH -1

--- TempConv/src/App.jsx
+++ TempConv/src/App.jsx
@@ -4,35 +4,42 @@ import './App.css'
 
 function App() {
   const[name, setName] = useState('')
-  const [color, setColor] = useState('orange')
-    useEffect(()=>{
-    document.body.style.color = color;
-    window.addEventListener('keydown', handleEnter);
+  const [color, setColor] = useState('grey')
+  
+  // useEffect(()=>{
+  //   document.body.style.backgroundColor = color;
+  //   window.addEventListener('keydown', handleEnter);
 
-    return(() => {
-      window.removeEventListener('keydown', handleEnter);
-    }
-    )
-
-  }, [color])
+  //   return(() => {
+  //     window.removeEventListener('keydown', handleEnter);
+  //   }
+  //   )
+  // }, [color])
   useEffect(()=>{
     localStorage.setItem('name', name)
   }, [name])
 
+  useEffect(()=>{
+    const myDiv = document.getElementById('myDivId');
+    myDiv.style.backgroundColor = color;
+    document.addEventListener('click', changeColor);
+    return(()=>{
+      document.removeEventListener('click', changeColor);
+    })
+  }, [color])
 
-  
-  
   function changeColor(){
     setColor('red');
   }
-
+color
   function handleEnter(event){
     if(event.keyCode === 13){
       setColor('red')
     }
   }
+  
   return (<>
-
+<div id='myDivId'style={{width:'200px', height:'200px'}}></div>
 <button onClick={changeColor}></button>
 <input onChange={(e)=>setName(e.target.value)}></input>
     </>
