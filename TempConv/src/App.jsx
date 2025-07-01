2b5166ccdb98b47e578f18f7c47cb0746c5d795e
Alfiya <al.yausheva@gmail.com>
2025-07-01 23:06:11 +0300

    ⊗jsrtPmHkURf/2

--- TempConv/src/App.jsx
+++ TempConv/src/App.jsx
@@ -8,17 +8,18 @@ import './App.css'
 
 function App() {
   const[state, setState] = useState('')
-  const ref = useRef(0);
+  const ref = useRef('');
 
 function handleClick(){
- setState(state + '!')
+ ref.current = ref.current + '!'
+ console.log(ref.current)
 }
 
   return (<>
 <button >
   state click: {ref.current}
 </button>
-<p>text{state}</p>
+<p>text{ref.current}</p>
 <button onClick={handleClick}>click</button>
     </>
   )
