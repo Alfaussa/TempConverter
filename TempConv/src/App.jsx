81488a0e5833ed7fa3da59b87c4df4aeaf889d61
Alfiya <al.yausheva@gmail.com>
2025-07-01 22:55:21 +0300

    ⊗jsrtPmHkURf

--- TempConv/src/App.jsx
+++ TempConv/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { MyContext } from './MyContext';
 import Parent from './Parent';
 import BigBox from './BigBox';
@@ -7,89 +7,20 @@ import './App.css'
 
 
 function App() {
-  // const[name, setName] = useState('')
-  // const [color, setColor] = useState('grey')
-  const[isVisible, setIsVisibility] = useState(false);
-  const [name, setName] = useState('small box');
-  const[age, setAge] = useState('50');
-  // useEffect(()=>{
-  //   document.body.style.backgroundColor = color;
-  //   window.addEventListener('keydown', handleEnter);
+  const[state, setState] = useState(0)
+  const ref = useRef(0);
 
-  //   return(() => {
-  //     window.removeEventListener('keydown', handleEnter);
-  //   }
-  //   )
-  // }, [color])
-  // useEffect(()=>{
-  //   localStorage.setItem('name', name)
-  // }, [name])
-
-  // useEffect(()=>{
-  //   const myDiv = document.getElementById('myDivId');
-  //   myDiv.style.backgroundColor = color;
-  //   document.addEventListener('click', changeColor);
-  //   return(()=>{
-  //     document.removeEventListener('click', changeColor);
-  //   })
-  // }, [color])
-let block;
-if(isVisible){
-  block = <div style={{
-    width:'100px', 
-    height:'100px', 
-    backgroundColor:'purple'}}>я появился!</div> 
+function handleClick(){
+  ref.current = ref.current + 1;
+  console.log('ref click: ' +ref.current);
 }
-useEffect(() => {
-{
- document.addEventListener('click', closeBlock);
 
-
- return() => {
- document.removeEventListener('click', closeBlock);
- }}}
-, [])
-
-//   function changeColor(){
-//     setColor('red');
-//   }
-// color
-//   function handleEnter(event){
-//     if(event.keyCode === 13){
-//       setColor('red')
-//     }
-//   }
-
- function handleBlock(event){
-    event.preventDefault();
-    setIsVisibility(!isVisible);
-  }
-  function closeBlock(event){
-      event.preventDefault();
-      const targetElement = event.target;
-if(targetElement.id !== 'link'){
-
-    setIsVisibility(false);}
-  }
-
-  const handleAge = () => {
-    const newAge = +age/2;
-    setAge(newAge)
-  }
   return (<>
-{/* <div id='myDivId'style={{width:'200px', height:'200px'}}></div>
-<button onClick={changeColor}></button> */}
-{/* <input onChange={(e)=>setName(e.target.value)}></input> */}
-<a id='link' href='#' onClick={handleBlock}>sylka</a>
-{block}
-<MyContext.Provider value={name}> 
-			<BigBox />
-      <button onClick={()=>{setName('metal box')}}>click</button>
-		</MyContext.Provider> 
-    <MyContext.Provider value = {age}>
-      <Parent/>
-      <button onClick ={handleAge}>click age</button>
-    </MyContext.Provider> 
+<button onClick={handleClick}>
+  state click: {ref.current}
+</button>
+<p>text</p>
+<button>click</button>
     </>
   )
 }
