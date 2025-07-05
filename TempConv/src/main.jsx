1c5b1771497f6d6839d442d429866279b2ada05c
Alfiya <al.yausheva@gmail.com>
2025-07-05 23:04:22 +0300

    ⊗jsrtPmRtRte

--- TempConv/src/main.jsx
+++ TempConv/src/main.jsx
@@ -5,6 +5,8 @@ import ReactDOM from  'react-dom/client';
 import {
 	createBrowserRouter,
 	RouterProvider,
+  createRoutesFromElements,
+  Route
 } from 'react-router-dom';
 
 
@@ -14,6 +16,16 @@ const router = createBrowserRouter([
 		element: <div>I'm number one in React!</div>, 
 	},
 ]);
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route
+//     path = ''
+//     element = "I'm number one in React!"
+// // 	},'
+//     />
+//   )
+// )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
